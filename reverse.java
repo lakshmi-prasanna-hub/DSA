053ac62d5ff1d612b18b09f90aecd466f8b09dc4
Vemanaboina Lakshmi Prasanna <vemanaboinalakshmiprasanna@gmail.com>
2026-02-16 11:32:25 +0530

    Implement integer reversal method in Solution class

---
+++ reverse.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        int rev=0;
+        while(x!=0){
+              if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            int rem=x%10;
+            rev=rev*10+rem;
+            x=x/10;
+        }
+        if(x<0)
+        return -rev;
+        return rev;
+    }
+}
