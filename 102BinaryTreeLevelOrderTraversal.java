29452d6cc48445399b3f5e3c7563a2c17f369d99
Vemanaboina Lakshmi Prasanna <vemanaboinalakshmiprasanna@gmail.com>
2026-02-19 14:37:21 +0530

    Implement level order traversal for binary tree

---
+++ 102BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ List<List<Integer>> result=new ArrayList<>();
+        if(root==null)
+        return result;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            List<Integer> currlevel=new ArrayList<>();
+            while(size-->0){
+                TreeNode curr=q.poll();
+                currlevel.add(curr.val);
+                if(curr.left!=null)
+                q.offer(curr.left);
+                if(curr.right!=null)
+                q.offer(curr.right);
+            }
+            result.add(currlevel);
+        }
+        return result;
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result=new ArrayList<>();
+        if(root==null)
+        return result;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            List<Integer> currlevel=new ArrayList<>();
+            while(size-->0){
+                TreeNode curr=q.poll();
+                currlevel.add(curr.val);
+                if(curr.left!=null)
+                q.offer(curr.left);
+                if(curr.right!=null)
+                q.offer(curr.right);
+            }
+            result.add(currlevel);
+        }
+        return result;
+    }
+}
